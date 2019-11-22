7e3c0dc4f595658b2eb340e89301e4d073b20864
MegiDervishi <megidervishi99@gmail.com>
2019-11-22 19:29:36 +0100

    274

---
+++ solutions/274.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        if n==0: return 0
+        #countsort
+        cs = [0 for _ in range(n+1)]
+        for c in citations:
+            cs[ min(n,c)] += 1
+        h = 0
+        for i in range(n,-1,-1):
+            h += cs[i]
+            if h >= i:
+                return i
+        return 0 #no index found
